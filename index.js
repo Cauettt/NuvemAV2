e2ce700a4c3201a31a5bbec4332c503055aa28bd
Cauettt <cauetrodrigues@gmail.com>
2025-04-28 17:03:09 -0300

    Update index.js

--- index.js
+++ index.js
@@ -16,12 +16,22 @@ app.get("/pesquisa/:parametro", function(req,res){
     res.send("retorno pesquisa:" + req.params.parametro);
 })
 
-app.get("/publicar/:nome", function (req,res){
+app.get("/publicar/:nome", function (req, res) {
     var nome = req.params.nome;
-    if(nome){
-        res.send("<h1>postagem "+nome+" publicada<h1>!");
-    }else{
-        res.send("postagem publicada")
-    }
+    res.send("<h1>postagem " + nome + " publicada</h1>!");
+});
+
+app.get("/publicar", function (req, res) {
+    res.send("postagem publicada");
 })
 
+    
+
+
+app.listen(process.env.PORT ?? 3000,function(erro){ 
+    if (erro){
+        console.log("Erro ao Iniciar.");
+    }else{
+        console.log("Servidor Iniciado.");
+    }
+})
